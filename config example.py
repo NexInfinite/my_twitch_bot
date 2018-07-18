45ce8ebe89d58491e5a3bdda8821f54ed9a90b82
NexInfinite <stormparardiseonline@gmail.com>
2018-07-18 18:39:06 +0100

    Stable version 1.0, read README for help

---
+++ config example.py
@@ -0,0 +1,7 @@
+channel_nameconfig = "Channel_You_Want_To_Connect_To"# This will be you channel, or a channel you are connecting too
+oauthconfig = "oauth:************" # You will find this here: https://twitchapps.com/tmi/
+nicknameconfig = "Channel_Wich_Has_Answers_The_Command"# This will be the channel you want to answer the command, i.e for me it would be nexchatbot
+client_idconfig = "************" # You don't need to add this but if you have an app on twitchdevs website add the 'client+id' in here
+
+serverconfig = 'irc.chat.twitch.tv' # The server you will connect too (Don't need to change)
+portconfig = 6667 # The port you will connect too (Don't need to change)
